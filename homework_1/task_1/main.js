40d2e9f869f8c6f0cfa1ebf5cc62f30edd2c9c91
Oleg Vinnikov <leess0347@gmail.com>
2016-11-10 18:57:02 +0200

    no message

---
+++ homework_1/task_1/main.js
@@ -0,0 +1,14 @@
+var arr = ['я', 'умею', 'писать', 'рекурсивные', 'функции'];
+var lengthArr = arr.length;
+
+function consoleRec(a){ 
+    if(1 < a ) { 
+        i=0;   
+        consoleRec(a - 1);
+        i++;
+    }    
+    console.log(arr[i]);  
+
+}
+consoleRec(lengthArr);
+
